dcef121b18b2046535503d268e127809f649b876
Claudia <claudiavanta290@gmail.com>
2022-11-14 15:52:47 +0200

    Created splash screen with a 3 second transition

--- EventHub/ContentView.swift
+++ EventHub/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            Text("ContentView")
+                .foregroundColor(.white)
+                .font(.system(size: 30))
         }
-        .padding()
     }
 }
 
